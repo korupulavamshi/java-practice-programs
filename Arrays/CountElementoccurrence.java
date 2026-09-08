f463b19dbe13931f15b8d81d1a1e72bbc1ea1f2c
Korupula Vamshi <korupulavamshi06@gmail.com>
2026-09-08 23:09:42 +0530

    Add CountElement class to count occurrences of elements

---
+++ Arrays/CountElementoccurrence.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class CountElement {
+
+	    public static void main(String[] args) {
+
+	        int num[] = {1, 2, 2, 2, 3, 4, 4};
+
+	        for (int i = 0; i < num.length; i++) {
+
+	            int count = 0;
+
+	            for (int j = 0; j < num.length; j++) {
+
+	                if (num[i] == num[j]) {
+	                    count++;
+	                }
+	            }
+
+	            if (count > 1) {
+	                System.out.println(num[i] + " occurs " + count + " times");
+	            }
+	        }
+	        
+	    }
+}
+
+/*output 
+2 occurs 3 times 
+4 occurs 2 times */
